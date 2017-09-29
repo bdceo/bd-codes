e70d44dfed12dcf62cf23ff9625f080bb62c0d40
Chenye Ding <dingchenye@juran.com.cn>
2017-09-29 14:31:21 +0800

    bf code

--- src/test/java/com/bdsoft/BF.java
+++ src/test/java/com/bdsoft/BF.java
@@ -5,6 +5,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import com.bdsoft.utils.http.BDHttpParam;
 import com.bdsoft.utils.http.BDHttpUtil;
 
 /**
@@ -17,17 +18,22 @@ import com.bdsoft.utils.http.BDHttpUtil;
 public class BF {
 
 //	static long[] uidArr = { 20738773, 20738772, 20738924, 20738801, 20738771, 20738939, 20736016, 20735592 };
-	static long[] uidArr = { 20730531, 20739042, 20739041, 20738551, 20730367, 20738812, 20738787, 20740445 };
+	static long[] uidArr = { 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 10010 };
 	static int rdRang = uidArr.length;
 	static Random rd = new Random(System.currentTimeMillis());
 
 	public static void main(String[] args) throws Exception {
+		for(int i=0;i<rdRang ;i++){
+			uidArr[i] = System.nanoTime() + rd.nextInt(1000);
+		}
+
 		// 地址
 //		String url = "http://alpha-www.gdfcx.net/member-app/v1/api/designers/#uid/score?offset=0&limit=12";
-		String url = "http://uat-www.gdfcx.net/member-app/v1/api/designers/#uid/score?offset=0&limit=12";
+		String url = "http://localhost:8090/api/v1/coupons/issueCoupons?activityId=111544887702192128&platform=10100";
 
 		// 并发数
-		int bfNum = 50;
+		int bfNum = 20;
+
 		CountDownLatch cdt = new CountDownLatch(1);
 		ExecutorService es = Executors.newCachedThreadPool();
 		for (int i = 0; i < bfNum; i++) {
@@ -45,8 +51,8 @@ public class BF {
 
 		public Bingfa(CountDownLatch cdt, String url) {
 			this.cdt = cdt;
-			// this.url = url;
-			this.url = url.replaceAll("#uid", String.valueOf(uidArr[rd.nextInt(rdRang)]));
+			 this.url = url;
+//			this.url = url.replaceAll("#uid", String.valueOf(uidArr[rd.nextInt(rdRang)]));
 		}
 
 		public void run() {
@@ -57,7 +63,10 @@ public class BF {
 				// 并发
 				System.out.println(String.format("线程>%s，请求>%s", name, url));
 				long tmp = System.currentTimeMillis();
-				BDHttpUtil.sendGet(url); 
+
+				BDHttpParam hp = BDHttpParam.init().addHeader("X-Member-Id",String.valueOf(uidArr[rd.nextInt(rdRang)]));
+				BDHttpUtil.sendPost(url, hp);
+
 				System.err.println(
 						String.format("线程>%s 执行完，调用comment-server耗时：%d", name, (System.currentTimeMillis() - tmp)));
 			} catch (Exception e) {
