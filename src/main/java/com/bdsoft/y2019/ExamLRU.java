870e4225d7f2aea5ddbb84a9636b860e63634873
Chenye Ding <chenyeding@huashenghaoche.com>
2019-08-15 14:31:11 +0800

    lru impl

--- src/main/java/com/bdsoft/y2019/ExamLRU.java
+++ src/main/java/com/bdsoft/y2019/ExamLRU.java
@@ -10,42 +10,54 @@ import java.util.Map;
 public class ExamLRU {
 
     public static void main(String[] args) {
-
-
+        ExamLRU lru = new ExamLRU();
+        int[] values = {4, 3, 2, 1, 4, 3, 5, 4, 3, 2, 1, 5, 4};
+        for (int v : values) {
+            lru.get(String.valueOf(v));
+            System.out.println();
+        }
+        System.out.println(lru.miss);
     }
 
-    int max = 100;
+    int miss = 0;
+    int max = 4;
     Map<String, Object> keyMap = new HashMap<>();
     LinkedList<String> keyList = new LinkedList<>();
 
-    public void put(String k, Object o) {
+    public Object get(String key) {
+        if (keyMap.containsKey(key)) {
+            keyList.remove(key);
+            keyList.addFirst(key);
+
+            return keyMap.get(key);
+        } else {
+            miss++;
+            System.out.println("Error: get key=" + key + ", miss=" + miss);
+            this.put(key, key);
+            return null;
+        }
+    }
+
+    public void put(String key, Object val) {
         // 如果满，删除尾部
         if (keyMap.size() == max) {
-            keyList.removeLast();
+            String rmKey = keyList.removeLast();
+            keyMap.remove(rmKey);
         }
 
         // 已存在：移除原始位置，放置链表头
-        if (keyMap.containsKey(k)) {
-            keyList.remove(k);
-            keyList.addFirst(k);
+        if (keyMap.containsKey(key)) {
+            keyList.remove(key);
+            keyList.addFirst(key);
         }
         // 不存在：直接放到头
         else {
-            keyList.addFirst(k);
+            keyList.addFirst(key);
         }
+
         // 更新缓存
-        keyMap.put(k, o);
-    }
-
-    public Object get(String k) {
-        if (keyMap.containsKey(k)) {
-            keyList.remove(k);
-            keyList.addFirst(k);
-
-            return keyMap.get(k);
-        }
-
-        return null;
+        keyMap.put(key, val);
+        System.out.println("put >> " + key + ", " + keyMap.keySet());
     }
 
 }
