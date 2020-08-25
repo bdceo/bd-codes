762370015c7a5dba723f865cd1b74826ca4e0a2f
chenyeding <chenyeding@huashenghaoche.com>
2020-08-25 18:21:46 +0800

    remark

--- src/main/java/com/bdsoft/bdceo/dp/factory/method/FactoryMethod.java
+++ src/main/java/com/bdsoft/bdceo/dp/factory/method/FactoryMethod.java
@@ -2,69 +2,67 @@ package com.bdsoft.bdceo.dp.factory.method;
 
 public class FactoryMethod {
 
-	// 工厂方法演示
-	// 参考：http://haolloyin.blog.51cto.com/1177454/332576
-	// http://alaric.iteye.com/blog/1908876
-	public static void main(String[] args) {
-		IProduct p = null;
+    // 工厂方法演示
+    // 参考：http://haolloyin.blog.51cto.com/1177454/332576
+    // http://alaric.iteye.com/blog/1908876
+    public static void main(String[] args) {
+        Factory fat = new ConcreteFactory();
 
-		Factory fat = new ConcreteFactory();
+        IProduct p = fat.createProduct(ConcreteProductA.class);
+        p.fun1();
 
-		p = fat.createProduct(ConcreteProductA.class);
-		p.fun1();
+        p = fat.createProduct(ConcreteProductB.class);
+        p.fun2();
+    }
 
-		p = fat.createProduct(ConcreteProductB.class);
-		p.fun2();
-	}
-
-}
-
-// 产品定义
-interface IProduct {
-
-	void fun1();
-
-	void fun2();
-}
-
-// 产品A实现
-class ConcreteProductA implements IProduct {
-
-	public void fun1() {
-		System.out.println(this.getClass().getSimpleName() + "-->fun1");
-	}
-
-	public void fun2() {
-		System.out.println(this.getClass().getSimpleName() + "-->fun2");
-	}
-}
-
-// 产品B实现
-class ConcreteProductB implements IProduct {
-
-	public void fun1() {
-		System.out.println(this.getClass().getSimpleName() + "-->fun1");
-	}
-
-	public void fun2() {
-		System.out.println(this.getClass().getSimpleName() + "-->fun2");
-	}
 }
 
 // 工厂方法，抽象类，定义创建对象的接口，
 abstract class Factory {
-	abstract <T extends IProduct> T createProduct(Class<T> c);
+    abstract <T extends IProduct> T createProduct(Class<T> c);
 }
 
 // 派生类，实现具体的对象创建逻辑
 class ConcreteFactory extends Factory {
 
-	public <T extends IProduct> T createProduct(Class<T> c) {
-		T product = null;
-		try {
-			product = (T) Class.forName(c.getName()).newInstance();
-		} catch (Exception e) {
-		}
-		return product;
-	}
+    public <T extends IProduct> T createProduct(Class<T> c) {
+        T product = null;
+        try {
+            product = (T) Class.forName(c.getName()).newInstance();
+        } catch (Exception e) {
+        }
+        return product;
+    }
+}
+
+// 产品定义
+interface IProduct {
+
+    void fun1();
+
+    void fun2();
+}
+
+// 产品A实现
+class ConcreteProductA implements IProduct {
+
+    public void fun1() {
+        System.out.println(this.getClass().getSimpleName() + "-->fun1");
+    }
+
+    public void fun2() {
+        System.out.println(this.getClass().getSimpleName() + "-->fun2");
+    }
+}
+
+// 产品B实现
+class ConcreteProductB implements IProduct {
+
+    public void fun1() {
+        System.out.println(this.getClass().getSimpleName() + "-->fun1");
+    }
+
+    public void fun2() {
+        System.out.println(this.getClass().getSimpleName() + "-->fun2");
+    }
 }
--- src/main/java/com/bdsoft/bdceo/dp/factory/nvwa/NvwaFactory.java
+++ src/main/java/com/bdsoft/bdceo/dp/factory/nvwa/NvwaFactory.java
@@ -3,42 +3,42 @@ package com.bdsoft.bdceo.dp.factory.nvwa;
 // 女娲造人、动物
 public interface NvwaFactory {
 
-	final String BASE = "com.bdsoft.bdceo.dp.factory.nvwa.";
+    String BASE = "com.bdsoft.bdceo.dp.factory.nvwa.";
 
-	Person genPerson(String type);
+    Person genPerson(String type);
 
-	Animal genAnimal(String type);
+    Animal genAnimal(String type);
 }
 
 // 阳性-男人
 class YangSheng implements NvwaFactory {
 
-	@Override
-	public Person genPerson(String type) {
-		System.out.println("YangSheng-genPerson" + type);
-		return new Man();
-	}
+    @Override
+    public Person genPerson(String type) {
+        System.out.println("YangSheng-genPerson" + type);
+        return new Man();
+    }
 
-	@Override
-	public Animal genAnimal(String type) {
-		System.out.println("YangSheng-genAnimal" + type);
-		return new Bull();
-	}
+    @Override
+    public Animal genAnimal(String type) {
+        System.out.println("YangSheng-genAnimal" + type);
+        return new Bull();
+    }
 
 }
 
 // 阴性-女人
 class YinSheng implements NvwaFactory {
 
-	@Override
-	public Person genPerson(String type) {
-		System.out.println("YinSheng-genPerson" + type);
-		return new Woman();
-	}
+    @Override
+    public Person genPerson(String type) {
+        System.out.println("YinSheng-genPerson" + type);
+        return new Woman();
+    }
 
-	@Override
-	public Animal genAnimal(String type) {
-		System.out.println("YinSheng-genAnimal" + type);
-		return new Cow();
-	}
+    @Override
+    public Animal genAnimal(String type) {
+        System.out.println("YinSheng-genAnimal" + type);
+        return new Cow();
+    }
 }
